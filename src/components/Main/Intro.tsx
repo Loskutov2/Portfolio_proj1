3e45f51aaed68c6102bb75cb4713a6f4cdb74383
elka <112241103+Loskutov2@users.noreply.github.com>
2025-01-09 18:20:21 +0200

    Update Intro.tsx

--- src/components/Main/Intro.tsx
+++ src/components/Main/Intro.tsx
@@ -2,6 +2,7 @@ import React from "react"
 export const Intro:React.FC = () => {
     return(
         <section className="container" style={{fontSize: '64px'}}>
+            I have not finished my web page yet :/
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique maxime molestiae, accusantium culpa, harum dolorem aut minus consequuntur laborum aliquam, vitae omnis laudantium. Cum unde quasi voluptatem similique, consectetur libero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique maxime molestiae, accusantium culpa, harum dolorem aut minus consequuntur laborum aliquam, vitae omnis laudantium. Cum unde quasi voluptatem similique, consectetur libero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique maxime molestiae, accusantium culpa, harum dolorem aut minus consequuntur laborum aliquam, vitae omnis laudantium. Cum unde quasi voluptatem similique, consectetur libero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique maxime molestiae, accusantium culpa, harum dolorem aut minus consequuntur laborum aliquam, vitae omnis laudantium. Cum unde quasi voluptatem similique, consectetur libero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique maxime molestiae, accusantium culpa, harum dolorem aut minus consequuntur laborum aliquam, vitae omnis laudantium. Cum unde quasi voluptatem similique, consectetur libero.
         </section>
     )
